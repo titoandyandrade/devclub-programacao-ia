45d12bcd7d925f43b51930e3b274c2bb97f433e4
titoandyandrade <andy.edu.andrade@gmail.com>
2026-08-04 23:11:44 -0300

    feat: arquivos das aulas 1 e 2

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* 
+Variável - Um pedacinho da memória do computador
+que eu guardo o que eu quiser
+let 
+console.log() - Mostra algo na tela
+
+Função - Pedacinho de código QUE
+Só executa QUANDO eu CHAMO ELA
+
+Lógica de Programação - Pensar como computador pensa
+Algoritmo - Receita de bolo
+
+[ ] Carregar a foto
+[ ] Enviar para a IA foto + orientação
+[ ] Receber a resposta da IA  
+[ ] Formatar a resposta
+[ ] Colocar na tela   
+     
+*/
+
+
+function lerFoto(){
+    console.log("Estou lendo a foto")
+}
